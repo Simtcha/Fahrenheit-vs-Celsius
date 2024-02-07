04db500544ad4cf7947f75ced1bb2d9808f4f161
Sim P <anomis.p@gmail.com>
2024-02-07 21:22:56 +0100

    some styling added

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ Pokud si ovšem neuvědomíte, že Američané používají pro měření teplot
 Nastudujte si na České Wikipedii jak se převádějí stupně Fahrenheita na stupně Celsia a vytvořte webovou stránku, která takový převod provede. 
 Vaše stránka se zeptá uživatele teplotu ve stupních Fahrenheita a pomocí document.body.innerHTML vypíše její ekvivalent ve stupních Celsia. */
 
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
 
 const stupneF = prompt("Zadej Fahrenheit stupně: ")
-stupneCelsia = Math.round((5 * (stupneF- 32))/9)
-document.body.innerHTML = stupneF + " Fahrenheita je přesně " + stupneCelsia + " stupeň/stupňů Celsia. "
+stupneCelsia = round(((5 * (stupneF- 32))/9),1)
+
+document.body.innerHTML += "<section>" + stupneF + " °F je přesně " + stupneCelsia + " °C. " + "</section>"
+
+if (stupneCelsia > 25) {
+    document.body.innerHTML += "<p>"+ "Sakra teplo";
+  } else if ((stupneCelsia < 25 && stupneCelsia  > 0)) {
+    document.body.innerHTML += "<p>"+ "To se dá";
+  } else { 
+    document.body.innerHTML += "<p>"+ "Sakra zima";
+  }
+  
